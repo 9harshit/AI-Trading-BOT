80b2e7be74ca5f57c549c22553973dea7fd66f86
9harshit <43147421+9harshit@users.noreply.github.com>
2020-11-28 18:09:57 +0530

    Code corrected for RNN using trading bot

--- aitrader_rnn.py
+++ aitrader_rnn.py
@@ -1,23 +1,21 @@
-import math
 import random
-import numpy as np
-import pandas as pd
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense
-from tensorflow.keras.layers import LSTM
-from tensorflow.keras.layers import Dropout
-
-from tqdm import  tqdm
 from collections import deque
 
+import numpy as np
+import pandas as pd
+from sklearn.preprocessing import MinMaxScaler
+from tensorflow.keras.layers import LSTM, Dense, Dropout
+from tensorflow.keras.models import Sequential
+from tqdm import tqdm
+
+
 class AI_Trader():
-    def __init__(self, state_size, action_space = 3, model_name = "AITRADER"): #HOLD, BUY, SELL
-       
+    def __init__(self, state_size, action_space = 3): #HOLD, BUY, SELL
+
         self.state_size = state_size
         self.action_space = action_space
         self.memory = deque(maxlen = 2000) #EXPERIENCE REPLACY
         self.inventory = [] #STOCKS WE OWN
-        self.model_name = model_name
 
         self.gamma = 0.90
         self.epsilon = 1.0
@@ -29,29 +27,29 @@ class AI_Trader():
     def model_builder(self):
 
         #MODEL WILL RETURN WILL ACTION TO PERFROM AND INPUT IS State/prices
-        
+
         # Initialising the RNN
         regressor = Sequential()
-        
+
         # Adding the first LSTM layer and some Dropout regularisation
-        regressor.add(LSTM(units = 75, return_sequences = True, input_shape = (state_reshaped.shape[1], 5)))
+        regressor.add(LSTM(units = 75, return_sequences = True, input_shape = self.state_size))
         regressor.add(Dropout(0.2))
-        
+
         # Adding a second LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 75, return_sequences = True))
         regressor.add(Dropout(0.2))
-        
+
         # Adding a third LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 75, return_sequences = True))
         regressor.add(Dropout(0.2))
-        
+
         # Adding a fourth LSTM layer and some Dropout regularisation
         regressor.add(LSTM(units = 75))
         regressor.add(Dropout(0.2))
-        
+
         # Adding the output layer
         regressor.add(Dense(units = self.action_space, activation = "linear"))
-        
+
         # Compiling the RNN
         regressor.compile(optimizer = 'adam', loss = 'mean_squared_error')#OUTPUT AS SELL, BUY, HOLD
 
@@ -63,9 +61,10 @@ class AI_Trader():
         #ACTION SELECTION , SELECT ACTION FROM THE MODEL OR RANDOM ACTION
 
         if random.random() <= self.epsilon:
-            return random.randrange(self.action_space) #returns random action 
+            return random.randrange(self.action_space) #returns random action
 
         action = self.model.predict(state)
+
         #WILL RETURN ACTION WITH HIGHEST PROBABILITY
         return np.argmax(action[0]) #0 because of output shape
 
@@ -82,96 +81,75 @@ class AI_Trader():
                 reward = reward + self.gamma * np.amax(self.model.predict(next_state)[0])
 
             target = self.model.predict(state)
+
             target[0][action] = reward
-            
+
             self.model.fit(state, target, epochs = 1, verbose = 0 )
 
         if self.epsilon > self.epsilon_final:
             self.epsilon *= self.epsilon_decay
 
-#TO GET REAL JUMP IN PRICES EVEN THOUGH PRICES ARE DIFFERNT
-def sigmoid(x):
-    return 1/ (1 + math.exp(-x))
 
-def stock_price_format(n):
-    if n < 0:
-        return "-${:.2f}".format(abs(n))
 
-    else:
-        return "${:.2f}".format(abs(n))
 
-def state_creator(data, timestep, window_size):
-    
-    starting_id = timestep - window_size +1
 
-    if starting_id >= 0:
-        windowed_data = data[starting_id:timestep+1,:]
-    else:
-        windowed_data = - starting_id * [data[0,:]] + list(data[0:timestep+1,:])
-        
-    return np.array(windowed_data)
-
-dataset_train = pd.read_csv('apple_1min.csv')
+episodes = 10
+batch_size = 32
+timestep = 12
+dataset_train = pd.read_csv('apple_5min.csv')
 dataset_train = dataset_train.dropna()
 training_set = dataset_train.iloc[:,1:].values
-# Feature Scaling
-from sklearn.preprocessing import MinMaxScaler
 sc = MinMaxScaler(feature_range = (0, 1))
 training_set_scaled = sc.fit_transform(training_set)
 
 
-def reshaping_state(data, rnn_tmstp):
-    state_reshaped = []
-    
-    for i in range(rnn_tmstp, (data.shape[0])):
-        state_reshaped.append(data[i-rnn_tmstp:i])
-        
-    state_reshaped = np.array(state_reshaped)
-    state_reshaped = np.reshape(state_reshaped, (state_reshaped.shape[0], state_reshaped.shape[1], 5))
-    return state_reshaped
+# Creating a data structure with 60 DAYsteps and 1 output
+X_train = []
+y_train = []
+for i in range(timestep, (training_set_scaled.shape[0])):
+    X_train.append(training_set_scaled[i-timestep:i])
+X_train = np.array(X_train)
 
-episodes = 10
-batch_size = 32
-data_samples  = len(training_set_scaled) - 1 
-window_size = 11
-timestep = 10
+prev_profit = 0
 
-state = state_creator(training_set_scaled, 0, window_size+1)
+# Reshaping
+X_train = np.reshape(X_train, (X_train.shape[0], X_train.shape[1], 5))
 
-state_reshaped = reshaping_state(state, timestep)
-trader = AI_Trader(window_size)
+data_samples = len(training_set_scaled) - 1
+
+trader = AI_Trader((X_train.shape[1],5))
 
 for episode in range(1, episodes+1):
     print("\nEpisodes :{}/{}".format(episode,episodes))
     total_porfit = 0
     trader.inventory = []
-    tran = 0 
+    tran = 0
 
-    state = state_creator(training_set_scaled, 0, window_size+1)
+    state = X_train[0:1,:]
 
-    state_reshaped = reshaping_state(state, timestep)
-    
+
+
     for t in tqdm(range(data_samples)):
-        
-        action = trader.trade(state_reshaped)
-        next_state = state_creator(training_set_scaled, t+1, window_size + 1)
 
-        next_state_reshaped = reshaping_state(next_state, timestep)
+        action = trader.trade(state)
+        next_state = X_train[t+1:t+2,:]
+
+        actual_state_value = sc.inverse_transform(state[0:1,-1,:])
 
         reward = 0
 
-        if action == 1 : 
-            trader.inventory.append(training_set[t,3])
+        if action == 1 :
+            trader.inventory.append(actual_state_value[-1,3])
             tran += 1
-            print("\nApple Stock Bought at " + stock_price_format(training_set[t,3]))
-                  
+            print("\nApple Stock Bought at " + (str(actual_state_value[-1,3])))
+
         elif action == 2 and len(trader.inventory) > 0:
             buy_price = trader.inventory.pop(0)
             tran += 1
 
-            reward = max(training_set[t,3] - buy_price, 0)
-            total_porfit += training_set[t,3] - buy_price
-            print("\nApple Stock Bought at " + stock_price_format(buy_price),"Sold at " + stock_price_format((training_set[t,3])),"Profit " + stock_price_format((training_set[t,3] - buy_price)))
+            reward = max((actual_state_value[-1,3] - buy_price), 0)
+            total_porfit += actual_state_value[-1,3] - buy_price
+            print("\nApple Stock Bought at " + str(buy_price)+ " Sold at " + str(actual_state_value[-1,3]),"Profit " + str(actual_state_value[-1,3] - buy_price))
 
         else :
             print("\nHolding Apple Stock")
@@ -180,17 +158,16 @@ for episode in range(1, episodes+1):
         else:
             done = False
 
-        trader.memory.append((state_reshaped, action, reward, next_state_reshaped, done))
-        state_reshaped = next_state_reshaped
-        
+        trader.memory.append((state, action, reward, next_state, done))
+        state = next_state
+
         print("Trans :", tran)
-        print("\nTOTAL PROFIT: {}".format(stock_price_format(total_porfit)))
+        print("\nTOTAL PROFIT: {}\n".format((total_porfit)))
+
+
 
         if done:
-           trader.model.save("trader1min_10.h5")
+            trader.model.save("trader_rnn_5min.h5")
+            print("***Training done***")
         if len(trader.memory) > batch_size:
             trader.batch_trade(batch_size)
-        
-
-
-
